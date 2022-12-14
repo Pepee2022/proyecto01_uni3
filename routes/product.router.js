0b7bfe0cb8ad162a7a847a0ded817aa424ee0191
Pepee1920 <017100363k@uandina.edu.pe>
2022-12-14 17:31:19 -0500

    Commit 14/12

--- routes/product.router.js
+++ routes/product.router.js
@@ -3,31 +3,27 @@ const router = express.Router();
 const ProductService = require('./../services/product.service');
 const service = new ProductService();
 
-
-
 router.get('/', (req, res) => {
   const products = service.find();
   res.status(200).json(products);
 });
 
-
 router.get('/filter', (req, res) => {
-    res.json({
-    message: '/products/filter'
+  res.json({
+    message: '/products/filter',
   });
 });
 
 router.get('/:id', (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   const product = service.finOne(id);
-  if (product === undefined){
+  if (product === undefined) {
     res.status(404).json({
-      message:'product no found',
-      id
-    })
+      message: 'product no found',
+      id,
+    });
   }
   res.status(200).json(product);
-
 });
 
 router.post('/', (req, res) => {
@@ -35,29 +31,27 @@ router.post('/', (req, res) => {
 
   const nuevoProducto = service.create(body);
   res.status(201).json({
-
     message: 'creado',
-    nuevoProducto
+    nuevoProducto,
   });
 });
 router.patch('/:id', (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   const body = req.body;
   const product = service.update(id, body);
   res.status(200).json({
     message: 'actualziado',
-    product
+    product,
   });
 });
 
 router.delete('/:id', (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   const eliminar = service.delete(id);
   res.status(200).json({
     message: 'eliminado',
-    eliminar
+    eliminar,
   });
 });
-
 
 module.exports = router;
