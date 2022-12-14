efe9b52ef978733ff65d7bd5c6381f621ea89bec
Pepee1920 <017100363k@uandina.edu.pe>
2022-12-14 18:42:53 -0500

    Commit 14/12

--- routes/index.js
+++ routes/index.js
@@ -5,34 +5,26 @@ const trabajadoresRouter = require('./trabajadores.router');
 const ventasRouter = require('./ventas.router');
 const clientesRouter = require('./clientes.router');
 
-function routerApi1(app) {
+function routerApi(app) {
   const routerV1 = express.Router();
   app.use('/api/v1', routerV1);
   routerV1.use('/products', productsRouter);
-}
 
-function routerApi2(app) {
   const routerV2 = express.Router();
   app.use('/api/v2', routerV2);
   routerV2.use('/paquetes', paquetesRouter);
-}
 
-function routerApi3(app) {
   const routerV3 = express.Router();
   app.use('/api/v3', routerV3);
   routerV3.use('/trabajadores', trabajadoresRouter);
-}
 
-function routerApi4(app) {
   const routerV4 = express.Router();
   app.use('/api/v4', routerV4);
   routerV4.use('/ventas', ventasRouter);
-}
 
-function routerApi5(app) {
   const routerV5 = express.Router();
   app.use('/api/v5', routerV5);
   routerV5.use('/clientes', clientesRouter);
 }
 
-module.exports = (routerApi1, routerApi2, routerApi3, routerApi4, routerApi5);
+module.exports = routerApi;
